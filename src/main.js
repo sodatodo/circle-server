62e160241adcc9ccd7efcfce79cb38ad55224e3c
WangSoda <wangsuda@icloud.com>
2021-07-05 19:25:55 +0800

    feat() test

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ const app = new Koa();
 app.use(bodyParser());
 
 app.use(async ctx => {
+    const { header } = ctx;
+    const eventType = header['x-github-event'];
     console.log(`ctx.header`, ctx.header);
-    console.log('ctx.body', ctx.request.body)
+    console.log('ctx.body', ctx.request.body);
     ctx.body = 'Hello Koa';
 });
 
