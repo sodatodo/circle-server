73977650ead5f4c8e5f47e8cda930e022dbe4a32
Soda Wang(王肃达) <CI25095@careerintlinc.local>
2021-07-05 15:22:47 +0800

    feat() add bodyparser

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
+app.use(bodyParser());
 
 app.use(async ctx => {
+    console.log(`ctx.header`, ctx.header);
+    console.log(`ctx.body`, ctx.body);
     ctx.body = 'Hello Koa';
 });
 
